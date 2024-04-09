8ed64c72caa6bf858db6eb0963d2cf3cda2d0119
GarinIvan <ivangarin08@gmail.com>
2024-04-09 22:08:23 +0300

    Commit 1
    Cool Commit

---
+++ Assets/Scripts/CardBuilding/Building.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+
+public class Building : MonoBehaviour
+{
+    [SerializeField] private Vector2 _buildingSize;
+    public Renderer _renderer;
+    public float health;
+
+    public Vector2 BuildingSize { get => _buildingSize; set {; } }
+
+    public void SetColor(bool isAvailableToBuild)
+    {
+        if (isAvailableToBuild)
+            _renderer.material.color = Color.green;
+        else
+            _renderer.material.color = Color.red;
+    }
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void ResetColor()
+    {
+        _renderer.material.color = Color.white;
+    }
+}
---
+++ Assets/Scripts/CardGun/GunCard.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunCard : MonoBehaviour
+{
+    public float Damage = 10f;
+    public float interval = 1f;
+    private float timer = 0f;
+    public Building color;
+    public Bullet bulletPrefab;
+    public Transform bulletSpawnPosition;
+
+    private void Update()
+    {
+        ShotFind();
+    }
+
+    private void ShotFind()
+    {
+        if (color._renderer.material.color == Color.white)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position + Vector3.up, transform.TransformDirection(Vector3.forward), out hit))
+            {
+                timer += Time.deltaTime;
+                if (hit.collider.gameObject.CompareTag("Enemy"))
+                {
+                    if (timer >= interval)
+                    {
+                        timer = 0f;
+                        Shot();
+                    }
+                }
+            }
+        }
+    }
+    private void Shot()
+    {
+        Instantiate(bulletPrefab, bulletSpawnPosition.position, transform.rotation);
+    }
+}
